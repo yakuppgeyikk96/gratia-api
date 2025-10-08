a759b0a09e5d8c98899e9fcc3e8d0371fe88801d
Yakup Geyik <yakupgeyik@Yakups-MacBook-Pro.local>
2025-10-09 00:35:33 +0300

    Update transport in nodemailer

--- src/shared/services/email.service.ts
+++ src/shared/services/email.service.ts
@@ -6,6 +6,14 @@ let transporter: Transporter | null = null;
 export const initializeEmailService = (): void => {
   transporter = createTransport({
     service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
+    tls: {
+      rejectUnauthorized: false,
+      ciphers: "SSLv3",
+    },
     auth: {
       user: process.env.EMAIL_USER || "",
       pass: process.env.EMAIL_PASS || "",
